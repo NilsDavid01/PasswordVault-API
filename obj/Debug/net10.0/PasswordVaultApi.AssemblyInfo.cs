f3b9cec3247b068c00061fdaef77f639a2b2fcdd
NilsDavid01 <mail@davida.pro>
2026-04-11 16:48:44 +0200

    update

--- obj/Debug/net10.0/PasswordVaultApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/PasswordVaultApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordVaultApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+722731950a79ee43d9fa266984440c975e9ee5ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordVaultApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordVaultApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
